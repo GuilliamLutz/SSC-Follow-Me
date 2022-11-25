23693b74c427f881a0203ba3539c316232d9b68d
RamonS <Ramonschone93@gmail.com>
2022-11-25 09:37:20 +0100

    Added mainclass object and main skeleton
    Basic layouting for program

---
+++ src/mainClass.h
@@ -0,0 +1,15 @@
+//****************************************************************************
+// TITLE		mainClass
+// DESCRIPTION	class definition of main program object
+// FILE			mainClass.h
+// AUTHOR		R. Schonewille
+// DATE			25/11/2022				
+// ***************************************************************************
+#pragma once
+
+class mainClass
+{
+public:
+
+	int run();
+};
