e8a5881a417896db57a11c60b50def8833c88e19
Guilliam <guilliamlutz@outlook.com>
2022-12-15 13:23:15 +0100

    Added files for straight follow function
    added the files from a fast test project. Goal now is to implement it in the project and make it working

---
+++ src/Actuators/straightfollow/src/motorControl.h
@@ -0,0 +1,30 @@
+
+#pragma once
+#include <ros/ros.h>
+#include "geometry_msgs/Twist.h"
+
+
+class Motorcontrol
+{
+private:
+    ros::Publisher motor_pub;
+    geometry_msgs::Twist forwardmsg;
+
+public:
+    Motorcontrol(ros::NodeHandle *nh)
+    {
+        motor_pub = nh->advertise<geometry_msgs::Twist>("/RosAria/cmd_vel", 10);
+    }
+
+    void driveForward()
+    {
+        forwardmsg.linear.x = 0.5;
+        motor_pub.publish(forwardmsg);
+    }
+
+    void resetDrive()
+    {
+        forwardmsg.linear.x = 0;
+        motor_pub.publish(forwardmsg);
+    }
+};
